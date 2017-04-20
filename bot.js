f94fc09ba52aac78484f6bd4707225e25e43e2b5
verget <verget@qip.ru>
2017-04-20 19:47:03 +0300

    make simple timeout function

--- bot.js
+++ bot.js
@@ -1,23 +1,47 @@
-var token = process.env.TOKEN;
+const token = process.env.TOKEN;
 
-var Bot = require('node-telegram-bot-api');
-var bot;
+const Bot = require('node-telegram-bot-api');
+let bot;
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
   bot.setWebHook(process.env.HEROKU_URL + bot.token);
-}
-else {
-  bot = new Bot(token, { polling: true });
+} else {
+  bot = new Bot(token, {polling: true});
 }
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-bot.onText(/^/, function (msg) {
-  var name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(function () {
-    // reply sent!
+bot.onText(/^/, function(msg) {
+  bot.sendMessage(msg.chat.id, new Date()).then(function(sentMessage) {
+    console.log(sentMessage);
+  
+    setInterval(() => {
+      bot.editMessageText(new Date(), {'chat_id' : msg.chat.id, 'message_id' : sentMessage.message_id}).then((response) => {
+        console.log(response);
+      });
+    }, 1000);
   });
+  
+});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const resp = match[1]; // the captured "whatever"
+  
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(msg.chat.id, resp);
+});
+
+bot.on('polling_error', (error) => {
+  console.log(error.code);  // => 'EFATAL'
+});
+
+bot.on('webhook_error', (error) => {
+  console.log(error.code);  // => 'EPARSE'
 });
 
 module.exports = bot;
