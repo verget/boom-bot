1171c6563cfbf4197b9509dd97ad4128c25e91c8
WilixLead <lead@w.wilix.ru>
2017-04-22 23:25:31 +0400

    few structure changes, few new functions, fs

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const token = process.env.TOKEN;
+const token = process.env.TOKEN || '334935256:AAHjOFyqCVLK5pdbZ98_TvZTepLg-jrt9NQ';//process.env.TOKEN;
 
 const fs = require('fs');
 const moment = require('moment');
@@ -14,50 +14,70 @@ if (process.env.NODE_ENV === 'production') {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
+const getTimer = (finishTime) => {
+  return new Promise((res, rej) => {
+    const now = moment();
+    let secondsAll = moment.unix(finishTime).diff(now, 'seconds');
+    let hoursDif = Math.floor(secondsAll / 1200);
+    let minutesDif = Math.floor((secondsAll - (hoursDif * 1200)) / 60);
+    let secondsDif = secondsAll - (hoursDif * 1200) - (minutesDif * 60);
 
-const showCurrentTimestamp = (chat_id) => {
-  return bot.sendMessage(chat_id, moment().unix()).then(function(sentMessage) {
-    setInterval(() => {
-      bot.editMessageText(moment().unix(), {'chat_id' : chat_id, 'message_id' : sentMessage.message_id}).then((response) => {
-      //here should be something
-      });
-    }, 1000);
+    if (hoursDif.toString().length < 2) hoursDif = '0' + hoursDif;
+    if (minutesDif.toString().length < 2) minutesDif = '0' + minutesDif;
+    if (secondsDif.toString().length < 2) secondsDif = '0' + secondsDif;
+    res({
+      h: hoursDif,
+      m: minutesDif,
+      s: secondsDif
+    });
   });
 };
 
 const showTimeDifference = (chat_id) => {
-  let config;
-  fs.readFile('config.json', 'utf8', function (err, data) {
-    if (err) throw err;
-    config = JSON.parse(data);
-    const now = moment();
-    
-    let secondsAll = moment.unix(config.next_year).diff(now, 'seconds');
-    let hoursDif = Math.floor(secondsAll / 1200);
-    let minutesDif = Math.floor((secondsAll - (hoursDif * 1200)) / 60);
-    let secondsDif = secondsAll - (hoursDif * 1200) - (minutesDif * 60);
-    
-    if (hoursDif.length < 2) hoursDif += '0' + hoursDif;
-    if (minutesDif.length < 2) minutesDif += '0' + minutesDif;
-    if (secondsDif.length < 2) secondsDif += '0' + secondsDif;
-    console.log(hoursDif + ':' + minutesDif + ':' + secondsDif);
-  });
-  
-  // return bot.sendMessage(chat_id, moment().unix()).then(function(sentMessage) {
-  //   setInterval(() => {
-  //     bot.editMessageText(moment().unix(), {'chat_id' : chat_id, 'message_id' : sentMessage.message_id}).then((response) => {
-  //       //here should be something
-  //     });
-  //   }, 1000);
-  // });
+  return new Promise((res, rej) => {
+    fs.readFile('users/' + chat_id + '.json', 'utf8', function (err, userData) {
+      if (err) throw('unknown user');
+      userData = JSON.parse(userData);
+      getTimer(userData.finishTime).then((timer) => {
+        console.log(timer);
+        bot.sendMessage(chat_id, timer.h + ':' + timer.m + ':' + timer.s).then((sentMessage) => {
+          setInterval(() => {
+            getTimer(userData.finishTime).then((timer) => {
+              bot.editMessageText(timer.h + ':' + timer.m + ':' + timer.s, {
+                'chat_id': chat_id, 'message_id': sentMessage.message_id
+              }).then((response) => {
+                //here should be something
+              })
+            })
+          }, 1000);
+        })
+      }).catch((err) => {
+        rej(err);
+      })
+    })
+  }).catch((err) => {
+    console.error(err);
+    return Promise.reject(err);
+  })
 };
 
-bot.onText(/\/time/, function(msg) {
-  showCurrentTimestamp(msg.chat.id);
+bot.onText(/\/dif/, function (msg) {
+  showTimeDifference(msg.chat.id);
 });
 
-bot.onText(/\/current_dif/, function(msg) {
-  showTimeDifference(msg.chat.id);
+bot.onText(/\/start/, function (msg) {
+  if (fs.existsSync('users/' + msg.chat.id + '.json')) { //check for user exist
+    bot.sendMessage(msg.chat.id, "Your game already started").then(() => {
+      return false;
+    });
+  }
+  let userObject = msg.from;
+  userObject.finishTime = moment().add(30, 'minutes').unix();
+  fs.writeFile('users/' + msg.chat.id + '.json', JSON.stringify(userObject), () => {
+    bot.sendMessage(msg.chat.id, "Your game just started " + userObject.first_name).then((message) => {
+      showTimeDifference(message.chat.id);
+    })
+  }); //create new user file with user data from Tg
 });
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -66,17 +86,17 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // of the message
 
   const resp = match[1]; // the captured "whatever"
-  
+
   // send back the matched "whatever" to the chat
   bot.sendMessage(msg.chat.id, resp);
 });
 
 bot.on('polling_error', (error) => {
-  console.log(error.code);  // => 'EFATAL'
+  console.error('polling_error ' + error.code);  // => 'EFATAL'
 });
 
 bot.on('webhook_error', (error) => {
-  console.log(error.code);  // => 'EPARSE'
+  console.error('webhook_error ' + error.code);  // => 'EPARSE'
 });
 
 module.exports = bot;
---
+++ env.cmd
@@ -0,0 +1,4 @@
+@echo=off
+setx var NODE_ENV=development
+setx var TOKEN=334935256:AAHjOFyqCVLK5pdbZ98_TvZTepLg-jrt9NQ
+setx var PORT=8888
