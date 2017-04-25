9b6b7981b188b0eaee5b5eaa5572bb406275f63b
verget <verget@qip.ru>
2017-04-25 20:03:31 +0300

    new interval logic

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 const token = process.env.TOKEN || '334935256:AAHjOFyqCVLK5pdbZ98_TvZTepLg-jrt9NQ';
-
 const fs = require('fs');
+const EventEmitter = require('events');
 const moment = require('moment');
 const Bot = require('node-telegram-bot-api');
+
+class MyEmitter extends EventEmitter {}
+const eventer = new MyEmitter();
+
 let bot;
+let users = [];
 
 if (process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
@@ -14,10 +19,51 @@ if (process.env.NODE_ENV === 'production') {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
+const globalInterval = () => {
+  let intervalId = setInterval(() => {
+    if (!users.length){
+      return false;
+    }
+    for (let user of users){
+      fs.readFile('users/' + user.chat_id + '.json', 'utf8', function (err, userData) {
+        let userObject = JSON.parse(userData);
+        if (userObject.finishTime && !err){
+          getTimer(userObject.finishTime).then((timer) => {
+            if (user.timer_message_id){
+              bot.editMessageText(timer.h + ':' + timer.m + ':' + timer.s, {
+                'chat_id': user.chat_id, 'message_id': user.timer_message_id
+              })
+            }else{
+              bot.sendMessage(user.chat_id, timer.h + ':' + timer.m + ':' + timer.s).then((sentMessage) => {
+                user.timer_message_id = sentMessage.message_id;
+              });
+            }
+            if (timer.zero){
+              users.splice(users.indexOf(user));
+            }
+          }).catch((err) => {
+            console.error(err || 'something wrong with timer');
+          })
+        }else{
+          console.error(err || 'something wrong with user');
+        }
+      })
+    }
+  }, 1000);
+  
+  eventer.on('game:stop', () => {
+    clearInterval(intervalId);
+  });
+  
+};
+
 const getTimer = (finishTime) => {
   return new Promise((res, rej) => {
     const now = moment();
     let secondsAll = moment.unix(finishTime).diff(now, 'seconds');
+    if (secondsAll < 0){
+      return res({h: '00', m: '00', s: '00', zero: true});
+    }
     let hoursDif = Math.floor(secondsAll / 3600);
     let minutesDif = Math.floor((secondsAll - (hoursDif * 3600)) / 60);
     let secondsDif = secondsAll - (hoursDif * 3600) - (minutesDif * 60);
@@ -25,91 +71,81 @@ const getTimer = (finishTime) => {
     if (hoursDif.toString().length < 2) hoursDif = '0' + hoursDif;
     if (minutesDif.toString().length < 2) minutesDif = '0' + minutesDif;
     if (secondsDif.toString().length < 2) secondsDif = '0' + secondsDif;
-    res({h: hoursDif, m: minutesDif, s: secondsDif});
+    return res({h: hoursDif, m: minutesDif, s: secondsDif, zero: false});
   });
 };
 
-const showTimeDifference = (chat_id) => {
-  return new Promise((res, rej) => {
-    fs.readFile('users/' + chat_id + '.json', 'utf8', function (err, userData) {
-      if (err) rej('unknown user');
-      userData = JSON.parse(userData);
-      console.log(moment(userData.finishTime));
-      getTimer(userData.finishTime).then((timer) => {
-        console.log(timer);
-        bot.sendMessage(chat_id, timer.h + ':' + timer.m + ':' + timer.s).then((sentMessage) => {
-          setInterval(() => {
-            getTimer(userData.finishTime).then((timer) => {
-              bot.editMessageText(timer.h + ':' + timer.m + ':' + timer.s, {
-                'chat_id': chat_id, 'message_id': sentMessage.message_id
-              }).then((response) => {
-                //here can be something
-              })
-            })
-          }, 1000);
-        })
-      }).catch((err) => {
-        rej(err);
-      })
+const userInit = (chat) => {
+  if (fs.existsSync('users/' + chat.id + '.json')) { //check for user exist
+    bot.sendMessage(chat.id, "Your game already started").then(() => {
+      if (!users.find((usr) => usr.chat_id === chat.id)){
+        users.push({
+          chat_id : chat.id
+        });
+      }
+      return false;
+    });
+  }
+  
+  let userObject = chat.from;
+  userObject.finishTime = moment().add(30, 'minutes').unix();
+  fs.writeFile('users/' + chat.id + '.json', JSON.stringify(userObject), () => {
+    bot.sendMessage(chat.id, "Your game just started " + userObject.first_name).then(() => {
+      users.push({
+        chat_id : chat.id
+      });
     })
-  }).catch((err) => {
-    console.error(err);
-    return Promise.reject(err);
-  })
+  }); //create new user file with user data from Tg
+  return false;
 };
 
-bot.onText(/\/dif/, function (msg) {
-  showTimeDifference(msg.chat.id);
+bot.onText(/\/stop/, () => {
+  eventer.emit('game:stop');
 });
 
 bot.onText(/\/start/, function (msg) {
-  if (fs.existsSync('users/' + msg.chat.id + '.json')) { //check for user exist
-    bot.sendMessage(msg.chat.id, "Your game already started");
-    return false;
+  userInit(msg.chat);
+});
+
+bot.onText(/\/add (.+)/, (msg, match) => {
+  const resp = match[1] * 1;
+  if(Number.isInteger(resp)){
+    if (fs.existsSync('users/' + msg.chat.id + '.json')) {
+      return fs.readFile('users/' +  msg.chat.id + '.json', 'utf8', function (err, userString) {
+        if (err){
+          console.error(err);
+          return false;
+        }
+        let userObject = JSON.parse(userString);
+        userObject.finishTime = moment.unix(userObject.finishTime).add(resp, 'seconds').unix();
+        fs.writeFile('users/' + msg.chat.id + '.json', JSON.stringify(userObject), () => {
+          bot.sendMessage(msg.chat.id, "Your timer updated " + userObject.first_name).then((message) => {
+            let currentUser = users.find((usr) => usr.chat_id === msg.chat.id);
+            if (!currentUser){
+              users.push({
+                chat_id : msg.chat.id
+              });
+            }else{
+              currentUser.timer_message_id = '';
+            }
+            return false;
+          })
+        });
+      })
+    }
   }
-  let userObject = msg.from;
-  userObject.finishTime = moment().add(30, 'minutes').unix();
-  fs.writeFile('users/' + msg.chat.id + '.json', JSON.stringify(userObject), () => {
-    bot.sendMessage(msg.chat.id, "Your game just started " + userObject.first_name).then((message) => {
-      showTimeDifference(message.chat.id);
-    })
-  }); //create new user file with user data from Tg
+  return bot.sendMessage(msg.chat.id, "No idea what you're mean");
 });
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-
+  
   const resp = match[1]; // the captured "whatever"
-
+  
   // send back the matched "whatever" to the chat
   bot.sendMessage(msg.chat.id, resp);
-});
-
-bot.onText(/\/add (.+)/, (msg, match) => {
-  const resp = match[1] *1;
-  console.log(resp);
-  if(Number.isInteger(resp)){
-    console.log('is number');
-    if (fs.existsSync('users/' + msg.chat.id + '.json')) {
-      fs.readFile('users/' +  msg.chat.id + '.json', 'utf8', function (err, userString) {
-        if (err){
-          return false;
-        }
-        let userObject = JSON.parse(userString);
-        console.log(moment(userObject.finishTime).add(resp, 'seconds').unix());
-        userObject.finishTime = moment(userObject.finishTime).add(resp, 'seconds').unix(); //todo negative numbers?
-        fs.writeFile('users/' + msg.chat.id + '.json', JSON.stringify(userObject), () => {
-          bot.sendMessage(msg.chat.id, "Your timer updated " + userObject.first_name).then((message) => {
-            showTimeDifference(message.chat.id);
-          })
-        });
-      })
-    }
-  }
-  bot.sendMessage(msg.chat.id, "No idea what you're mean");
-  return false;
 });
 
 bot.on('polling_error', (error) => {
@@ -119,5 +155,7 @@ bot.on('polling_error', (error) => {
 bot.on('webhook_error', (error) => {
   console.error('webhook_error ' + error.code);  // => 'EPARSE'
 });
+
+globalInterval();
 
 module.exports = bot;
