883d3c2c4a5375bbf17f83acc5a5128c7d0ce0d2
WilixLead <lead@w.wilix.ru>
2017-05-09 16:10:07 +0400

    make setter & getter for codes

--- services/code.js
+++ services/code.js
@@ -1,47 +1,58 @@
 const fs = require('fs');
 
 class codeService {
-  constructor() {
-    this.codeBase = JSON.parse(fs.readFileSync('storage/codes.json', 'utf8'));
+
+  getCode(codeString) {
+    let codeObject = this.codeBase.find((cd) => cd.string === codeString);
+    if (codeObject)
+      return codeObject ? Promise.resolve(codeObject) : Promise.reject();
   }
-  
-  saveCodes() {
-    return new Promise((res, rej) => {
-      return fs.writeFile('codes.json', JSON.stringify(this.codeBase), (err) => {
-        if (err) {
-          return rej(err);
-        }
-        return res();
+
+  newCode(codeString, codeValue) {
+    return this.getCode(codeString).then((codeObject) => {
+      return Promise.reject('Already added');
+    }).catch(() => {
+      let codeBase = this.codeBase;
+      codeBase.push({
+        "string": codeString,
+        "value": codeValue
+      });
+      this.codeBase = codeBase;
+      return Promise.resolve({
+        "string": codeString,
+        "value": codeValue
       });
     })
   }
-  
-  newCode(codeString, codeValue) {
-    let double = this.codeBase.find((cd) => cd.string === codeString);
-    if (double) {
-      return Promise.reject(codeString);
-    }
-    this.codeBase.push({
-      "string": codeString,
-      "value": codeValue
-    });
-    return this.saveCodes();
-  }
-  
-  
+
   changeCode(codeString, codeValue = null) {
     return new Promise((res, rej) => {
-      let found = this.codeBase.find((cd) => cd.string === codeString);
-      if (!found) {
+      this.getCode(codeString).then((found) => {
+        let codeBase = this.codeBase;
+        if (!codeValue) {
+          codeBase.splice(codeBase.indexOf(found));
+        } else {
+          found.value = codeValue; //mutable?
+        }
+        this.codeBase = codeBase;
+        return res();
+      }).catch(() => {
         return rej("Code not found");
-      }
-      if (!codeValue) {
-        this.codeBase.splice(this.codeBase.indexOf(found));
-      } else {
-        found.value = codeValue;
-      }
-      return this.saveCodes().then(res).catch(rej);
+      })
     });
   }
+
+  get codeBase() {
+    return JSON.parse(fs.readFileSync('storage/codes.json', 'utf8'));
+  }
+
+  set codeBase(value) {
+    fs.writeFile('storage/codes.json', JSON.stringify(value), (err) => {
+      if (err) {
+        console.error(err);
+      }
+    })
+  }
+
 }
 module.exports = new codeService();
